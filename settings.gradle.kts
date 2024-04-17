0fe1b3922d86388cb6dbd70888bda5dab7dcd83e
Jodis <132891555+Jodis974@users.noreply.github.com>
2024-04-17 15:49:53 +0400

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io") // Ajouter cette ligne
+    }
+}
+
+rootProject.name = "My Application"
+include(":app")
